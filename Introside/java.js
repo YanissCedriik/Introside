e46045e52fb9d0b30e5dafb8aaafff49066506cf
YanissCedriik <167817702+YanissCedriik@users.noreply.github.com>
2024-08-16 13:02:37 +0200

    Finished Website

--- Introside/java.js
+++ Introside/java.js
@@ -1,14 +1,56 @@
-function closeTab1(){
-document.getElementById("Tab1").innerHtml = "0" ;
+function closeTab1() {
+  if (document.getElementById("Tab1").innerHTML == "") {
+    openTab1();
+  } else document.getElementById("Tab1").innerHTML = "";
 }
 
-function closeTab2(){
-    document.getElementById("Tab2").innerHtml = "0" ;
-    }
+function closeTab2() {
+  if (document.getElementById("Tab2").innerHTML == "") {
+    openTab2();
+  } else document.getElementById("Tab2").innerHTML = "";
+}
 
-    function closeTab3(){
-        document.getElementById("Tab3").innerHtml = "0" ;
-        }
+function closeTab3() {
+  if (document.getElementById("Tab3").innerHTML == "") {
+    openTab3();
+  } else document.getElementById("Tab3").innerHTML = "";
+}
 
+function openTab1() {
+  document.getElementById("Tab1").innerHTML = /*HTML*/ `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            scelerisque metus eget aliquet convallis. Ut pulvinar ac lacus a
+            gravida. Vestibulum ante ipsum primis in faucibus orci luctus et
+            ultrices posuere cubilia curae; Maecenas tristique finibus orci sit amet
+            euismod. Pellentesque nec risus quis sem rhoncus malesuada. Quisque
+            scelerisque mauris vel nisl viverra, non semper sem gravida.
+    `;
+}
 
-        
+function openTab2() {
+  document.getElementById("Tab2").innerHTML = /*HTML*/ `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            scelerisque metus eget aliquet convallis. Ut pulvinar ac lacus a
+            gravida. Vestibulum ante ipsum primis in faucibus orci luctus et
+            ultrices posuere cubilia curae; Maecenas tristique finibus orci sit amet
+            euismod. Pellentesque nec risus quis sem rhoncus malesuada. Quisque
+            scelerisque mauris vel nisl viverra, non semper sem gravida.Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Vivamus
+            scelerisque metus eget aliquet convallis. Ut pulvinar ac lacus a
+            gravida. Vestibulum ante ipsum primis in faucibus orci luctus et
+            ultrices posuere cubilia curae; Maecenas tristique finibus orci sit amet
+            euismod. Pellentesque nec risus quis sem rhoncus malesuada. Quisque
+            scelerisque mauris vel nisl viverra, non semper sem gravida.
+    `;
+}
+
+function openTab3() {
+  document.getElementById("Tab3").innerHTML = /*HTML*/ `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+  scelerisque metus eget aliquet convallis. Ut pulvinar ac lacus a
+  gravida. Vestibulum ante ipsum primis in faucibus orci luctus et
+  ultrices posuere cubilia curae; Maecenas tristique finibus orci sit amet
+  euismod. Pellentesque nec risus quis sem rhoncus malesuada. Quisque
+  scelerisque mauris vel nisl viverra, non semper sem gravida.
+`;
+}
